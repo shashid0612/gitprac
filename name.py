37788fa715bf84f4eee617b8a2c5d7dd2cc2c8fe
shashid0612 <shashikaladebariki@gmail.com>
2021-07-11 12:49:50 +0000

    new branch

---
+++ name.py
@@ -0,0 +1,10 @@
+myName='Shashi'
+myAge=10
+myHeight='5'
+myWeight=20
+myHair='black'
+print(f"Let's talk about {myName}.")
+print(f"Let's talk about {myAge}.")
+print(f"Let's talk about {myHeight}.")
+print(f"Let's talk about {myWeight}.")
+print(f"Let's talk about {myHair}.")
